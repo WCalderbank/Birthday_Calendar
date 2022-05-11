c220342bf074ff13edc7a4bff6e0d67901101b7f
WCalderbank <wcalderbank99@gmail.com>
2022-05-11 12:32:50 +0100

    bday data created

---
+++ birthday_calendar/src/Components/Birthday_data.js
@@ -0,0 +1,34 @@
+let Birthday_data = [
+  {
+    id: 1,
+    name: "Beth Field",
+    age: 25,
+    image: "URL",
+  },
+  {
+    id: 2,
+    name: "Frank Reynold",
+    age: 41,
+    image: "URL",
+  },
+  {
+    id: 3,
+    name: "James Brook",
+    age: 37,
+    image: "URL",
+  },
+  {
+    id: 4,
+    name: "Lizzie Wick",
+    age: 19,
+    image: "URL",
+  },
+  {
+    id: 5,
+    name: "Walter Jones",
+    age: 21,
+    image: "URL",
+  },
+];
+
+export default Birthday_data;
