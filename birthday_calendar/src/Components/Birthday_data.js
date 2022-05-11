a7395934778ebad8815b0bd9a2f07bdf9fd2f78a
WCalderbank <wcalderbank99@gmail.com>
2022-05-11 16:02:03 +0100

    Individual Profile Pics

--- birthday_calendar/src/Components/Birthday_data.js
+++ birthday_calendar/src/Components/Birthday_data.js
@@ -1,28 +1,38 @@
+import Frank from "../Images/Frank.jpg";
+import Beth from "../Images/Lime Hat.png";
+import James from "../Images/James.jpg";
+import Lizzie from "../Images/Puppy.jpeg";
+import Walter from "../Images/Maul.jpeg";
 let Birthday_data = [
   {
     id: 1,
     name: "Beth Field",
     age: 25,
+    snap: Beth,
   },
   {
     id: 2,
     name: "Frank Reynold",
     age: 41,
+    snap: Frank,
   },
   {
     id: 3,
     name: "James Brook",
     age: 37,
+    snap: James,
   },
   {
     id: 4,
     name: "Lizzie Wick",
     age: 19,
+    snap: Lizzie,
   },
   {
     id: 5,
     name: "Walter Jones",
     age: 21,
+    snap: Walter,
   },
 ];
 
--- birthday_calendar/src/Components/List.js
+++ birthday_calendar/src/Components/List.js
@@ -1,16 +1,15 @@
 import React from "react";
 import "./List.css";
-import User from "../Images/user.jpeg";
 
 const List = ({ birthdays }) => {
   return (
     <>
       <div className="list">
         {birthdays.map((birthday) => {
-          const { id, name, age } = birthday;
+          const { id, name, age, snap } = birthday;
           return (
             <article key={id} className="birthday">
-              <img src={User} alt={name} />
+              <img src={snap} alt={name} />
               <div className="box">
                 <h4>{name}</h4>
                 <p>{age} years old</p>
