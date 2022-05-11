aa188bd74268d1c1f96960e8d5864fedc4900f96
WCalderbank <wcalderbank99@gmail.com>
2022-05-11 14:52:56 +0100

    styled

--- birthday_calendar/src/Components/Birthday_data.js
+++ birthday_calendar/src/Components/Birthday_data.js
@@ -1,34 +1,28 @@
-import Frank from "../Images/Frank.jpg";
 let Birthday_data = [
   {
     id: 1,
     name: "Beth Field",
     age: 25,
-    image: ,
   },
   {
     id: 2,
     name: "Frank Reynold",
     age: 41,
-    image: "URL",
   },
   {
     id: 3,
     name: "James Brook",
     age: 37,
-    image: "URL",
   },
   {
     id: 4,
     name: "Lizzie Wick",
     age: 19,
-    image: "URL",
   },
   {
     id: 5,
     name: "Walter Jones",
     age: 21,
-    image: "URL",
   },
 ];
 
